5fcbb6faf7442aae410ee3cf47843e51c44e2f9a
Suyash-Gandhi <suyash25gandhi@gmail.com>
2024-12-31 04:57:03 +0530

    netflix-clone

--- frontend/src/utils/firebase-config.js
+++ frontend/src/utils/firebase-config.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAosDbJF5PB14cgwLjp-jpWv5ph6CJJeyQ",
-  authDomain: "netflix-clone-d3934.firebaseapp.com",
-  projectId: "netflix-clone-d3934",
-  storageBucket: "netflix-clone-d3934.firebasestorage.app",
-  messagingSenderId: "581480365749",
-  appId: "1:581480365749:web:54992a87cbf60f4d3808fc",
-  measurementId: "G-KLMH3FCYDX"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
